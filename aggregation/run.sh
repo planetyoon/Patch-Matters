573ef95f02e4ffb55bac59fccca8675c77449e1c
planetyoon <planetyoon@naver.com>
2025-06-25 07:35:02 +0000

    Fix: update run.sh on /aggregation

--- aggregation/run.sh
+++ aggregation/run.sh
@@ -2,27 +2,27 @@ set -ex
 
 # export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
 node_index=0
-node_num=8
+node_num=1 # Use 1 for single node, or set to the number of nodes if running on multiple nodes
 chunk_num=8
 
 
-for (( chunk_index=0; chunk_index<=$[$chunk_num-1]; chunk_index++ ))
+for (( chunk_index=0; chunk_index<$chunk_num; chunk_index++ ))
 do
     # Assign each process to a unique GPU using chunk_index
     gpu_id=$chunk_index  # Assuming chunk_index corresponds to a GPU (e.g., 0, 1, 2 for 3 GPUs)
     
     # Set CUDA_VISIBLE_DEVICES to assign a unique GPU to each process
-    CUDA_VISIBLE_DEVICES=$gpu_id nohup python3 /aggregation/main.py \
-        --input_data /description_generate/description_output.json \
-        --output_folder /aggregation/ \
+    CUDA_VISIBLE_DEVICES=0 python3 /root/Patch-Matters/aggregation/main.py \
+        --input_data /root/Patch-Matters/description_generate/description_output.json \
+        --output_folder /root/Patch-Matters/aggregation/ \
         --chunk_index $chunk_index \
         --chunk_num $chunk_num \
         --node_index $node_index \
-        --node_num $node_num > /Patch-Matters/aggregation/$chunk_index.log 2>&1 &
+        --node_num $node_num
 done
 
 wait
 
-python3 aggregation/combine.py \
-    --folder_path /aggregation \
-    --output_file /aggregation/aggregation_output.json
+python3 /root/Patch-Matters/aggregation/combine.py \
+    --folder_path /root/Patch-Matters/aggregation \
+    --output_file /root/Patch-Matters/aggregation/aggregation_output.json
